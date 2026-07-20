0f58ad291e3410e0ba8318a63f5fe66a7792a540
Rosa <romorale@student.42malaga.com>
2026-07-20 19:14:04 +0200

    basic argument checking in main + makefile

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME = ircserv
+
+SRC_DIR = ./Srcs/
+OBJ_DIR = obj/
+SRC =	main.cpp\
+#		server.cpp\
+#		client.cpp\
+
+OBJS	= $(addprefix $(OBJ_DIR), $(SRC:.cpp=.o))
+
+CC = c++
+CFLAGS = -Wall -Wextra -Werror -g -std=c++98
+RM = rm -f
+
+$(OBJ_DIR)%.o:	$(SRC_DIR)%.cpp
+	@mkdir -p $(OBJ_DIR)
+	$(CC) ${CFLAGS} -o $@ -c $<
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+	@echo '$(NAME) compiled successfully!'
+
+all: $(NAME)
+
+fclean: clean
+	@$(RM) $(NAME)
+	@echo '$(NAME) deleted!'
+
+clean:
+	@$(RM) $(OBJS)
+	@echo 'obj deleted!'
+
+re: fclean all
+
+.PHONY : all clean fclean re
---
+++ Srcs/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+//#include <string>
+//#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <exception>
+#include <csignal>
+
+//#include <cctype>
+#include <cstdlib>
+
+static void		handleSignal(int signal)
+{
+	if (signal == SIGINT || signal == SIGQUIT)
+		std::cout << "Bye bye!" << std::endl;
+	exit(0);
+}
+
+void	argChecker(char **av)
+{
+	std::string portStr = av[1];
+
+	int	portNum = atoi(av[1]);
+
+	if (portStr.find_first_not_of("0123456789") != std::string::npos)
+		throw std::invalid_argument("Port should be a number, please!");
+
+	if (portNum < 1024 || portNum > 65535)
+		throw std::invalid_argument("Invalid port number! Please use a number between 1024 and 65535.");
+
+	int i = 0;
+	while(av[2][i])
+	{
+		if (!(isalnum(av[2][i])))
+		{
+			throw std::invalid_argument("Password should be alphanumeric, please!");
+			return ;
+		}
+		i++;
+	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		std::cerr << "Wrong arguments!"<< std::endl;
+		std::cerr << "Should be: ./ircserv <port number> <password>"<< std::endl;
+		return (1);
+	}
+
+	std::string port = av[1];
+	std::string pass = av[2];
+
+	try
+	{
+		signal(SIGINT, handleSignal);
+		signal(SIGQUIT, handleSignal);
+		std::cout << "\nPort number: " << port << "\nPassword: " << pass << "\n"<< std::endl;
+		argChecker(av);
+		//server starts here
+		std::cout << "All args are valid, server starts here." << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		//close fds here
+		std::cerr << "ERROR: " << e.what() << std::endl;
+		return (1);
+	}
+	//when the loop is stopped and the server is closed
+	std::cout << "the server is closed!" << std::endl;
+	return (0);
+}
